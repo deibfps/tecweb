89c219b9993fb56da021bd90111939a2df2a2a17
Deib <id1386859@gmail.com>
2025-01-29 19:56:21 -0600

    Se agregó el archivo pie.inc.php

---
+++ Practicas/p03/inclusion_archivos/pie.inc.php
@@ -0,0 +1,11 @@
+<hr/>
+<?php
+echo "<div><h1 style=\"border-width:3px;border-style:groove; background-color:#ffcc99;\">
+Final de la página PHP Vínculos útiles: <a href=\"https://php.net\">php.net</a>
+&nbsp; <a href=\"https://mysql.org\">mysql.org</a></h1></div>";
+
+echo "Nombre del archivo ejecutado: " . $_SERVER['PHP_SELF'] . "&nbsp;&nbsp;&nbsp;";
+echo "Nombre del archivo incluido: " . __FILE__;
+?>
+</body>
+</html>
