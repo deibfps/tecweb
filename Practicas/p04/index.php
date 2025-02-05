97b39744d50108f055bf7a0f3e83bda18c6e7d73
Deib <id1386859@gmail.com>
2025-02-04 22:25:28 -0600

    Se agregó el ejercicio 4 al index.php

--- Practicas/p04/index.php
+++ Practicas/p04/index.php
@@ -63,57 +63,41 @@
         echo "<p>Luego, al hacer \$b = &\$a, ahora \$b también apunta a la misma memoria que \$a, por lo que su valor también se actualiza automáticamente.</p>";
     ?>
 
-<h2>Ejercicio 3</h2>
+    <h2>Ejercicio 3</h2>
     <p>Seguimiento del contenido y tipo de variables después de cada asignación:</p>
 
     <?php
         $a = "PHP5";
-        echo "<h4>Después de \$a = \"PHP5\";</h4>";
-        echo "<ul>";
-        echo "<li>\$a = $a (Tipo: " . gettype($a) . ")</li>";
-        echo "</ul>";
-
         $z[] = &$a;
-        echo "<h4>Después de \$z[] = &\$a;</h4>";
-        echo "<ul>";
-        echo "<li>\$z[0] = {$z[0]} (Tipo: " . gettype($z[0]) . ")</li>";
-        echo "</ul>";
-
         $b = "5a version de PHP";
-        echo "<h4>Después de \$b = \"5a version de PHP\";</h4>";
-        echo "<ul>";
-        echo "<li>\$b = $b (Tipo: " . gettype($b) . ")</li>";
-        echo "</ul>";
-
         $c = $b * 10;
-        echo "<h4>Después de \$c = \$b * 10;</h4>";
-        echo "<ul>";
-        echo "<li>\$c = $c (Tipo: " . gettype($c) . ")</li>";
-        echo "<li>PHP intentó convertir el string '\$b' en número, pero como comienza con '5', lo interpreta como 5 y multiplica 5 * 10 = 50.</li>";
-        echo "</ul>";
-
         $a .= $b;
-        echo "<h4>Después de \$a .= \$b;</h4>";
-        echo "<ul>";
-        echo "<li>\$a = $a (Tipo: " . gettype($a) . ")</li>";
-        echo "<li>Como \$a era 'PHP5' y \$b era '5a version de PHP', la concatenación resulta en 'PHP55a version de PHP'.</li>";
-        echo "</ul>";
-
         $b *= $c;
-        echo "<h4>Después de \$b *= \$c;</h4>";
-        echo "<ul>";
-        echo "<li>\$b = $b (Tipo: " . gettype($b) . ")</li>";
-        echo "<li>PHP convierte \$b en un número (5) y hace 5 * 50 = 250.</li>";
-        echo "</ul>";
-
         $z[0] = "MySQL";
-        echo "<h4>Después de \$z[0] = \"MySQL\";</h4>";
-        echo "<ul>";
-        echo "<li>\$z[0] = {$z[0]} (Tipo: " . gettype($z[0]) . ")</li>";
-        echo "<li>\$a también cambia a 'MySQL' porque \$z[0] era una referencia a \$a.</li>";
-        echo "<li>\$a = $a</li>";
-        echo "</ul>";
-    ?>
 
+        function mostrar_con_globals() {
+            echo "<h4>Valores usando \$GLOBALS:</h4>";
+            echo "<ul>";
+            echo "<li>\$a = " . $GLOBALS['a'] . " (Tipo: " . gettype($GLOBALS['a']) . ")</li>";
+            echo "<li>\$b = " . $GLOBALS['b'] . " (Tipo: " . gettype($GLOBALS['b']) . ")</li>";
+            echo "<li>\$c = " . $GLOBALS['c'] . " (Tipo: " . gettype($GLOBALS['c']) . ")</li>";
+            echo "<li>\$z[0] = " . $GLOBALS['z'][0] . " (Tipo: " . gettype($GLOBALS['z'][0]) . ")</li>";
+            echo "</ul>";
+        }
+
+        function mostrar_con_global() {
+            global $a, $b, $c, $z;
+            echo "<h4>Valores usando global:</h4>";
+            echo "<ul>";
+            echo "<li>\$a = $a (Tipo: " . gettype($a) . ")</li>";
+            echo "<li>\$b = $b (Tipo: " . gettype($b) . ")</li>";
+            echo "<li>\$c = $c (Tipo: " . gettype($c) . ")</li>";
+            echo "<li>\$z[0] = {$z[0]} (Tipo: " . gettype($z[0]) . ")</li>";
+            echo "</ul>";
+        }
+
+        mostrar_con_globals();
+        mostrar_con_global();
+    ?>
 </body>
 </html>
