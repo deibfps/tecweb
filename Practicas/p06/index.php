71443a5b768c309955a8734f7f12959f0ed6c990
Deib <id1386859@gmail.com>
2025-02-06 20:43:42 -0600

    Se agregó ejercicio 1 a index.php

--- Practicas/p06/index.php
+++ Practicas/p06/index.php
@@ -6,37 +6,25 @@
     <title>Práctica 4</title>
 </head>
 <body>
+
+<!--Ejercicio 1-->
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+        require_once __DIR__ . '/src/funciones.php';
+        if (isset($_GET['numero']) && is_numeric($_GET['numero'])) {
+            $num = intval($_GET['numero']);
+            es_multiplo($num);
         }
     ?>
-
-    <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
+    <form method="get">
+        <label for="numero">Ingresa un número:</label>
+        <input type="text" name="numero" id="numero">
+        <input type="submit" value="Comprobar">
     </form>
-    <br>
-    <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
-        {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
-        }
-    ?>
+    <hr>
+<!--Ejercicio 1-->
+
 </body>
 </html>
