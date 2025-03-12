367e637b77b2ccbce30d128da7660eb011b26ce5
deibfps <id1386859@gmail.com>
2025-03-12 16:58:38 -0600

    Se realizaron ejemplos, clase 12/03/25

--- ejemplos/php/POO/03-constructor_namespace/Cabecera.php
+++ ejemplos/php/POO/03-constructor_namespace/Cabecera.php
@@ -1,5 +1,5 @@
 <?php
-namespace EJEMPLO\POO;
+namespace EJEMPLOS\POO;
 
 class Cabecera {
     private $titulo;
@@ -14,7 +14,28 @@ class Cabecera {
         $estilo = 'font-size: 40px; text-align: '.$this->ubicacion;
         echo '<div style="'.$estilo. '">';
         echo '<h4>'.$this->titulo.'</h4>';
-        echo '</div>'
+        echo '</div>';
+    }
+}
+
+class Cabecera2 {
+    private $titulo;
+    private $ubicacion;
+    private $enlace;
+
+    public function __construct($title, $location, $link) {
+        $this->titulo = $title;
+        $this->ubicacion = $location;
+        $this->enlace = $link;
+    }
+
+    public function graficar () {
+        $estilo = 'font-size: 40px; text-align: '.$this->ubicacion;
+        echo '<div style="'.$estilo. '">';
+        echo '<h4>';
+        echo '<a href="'.$this->enlace. '">'.$this->titulo.'</a>';
+        echo '</h4>';
+        echo '</div>';
     }
 }
 ?>
